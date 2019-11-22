8f5d9a4bcb330c371f987da9277ca3ccd4e1a4b5
hipak <hpk393@gmail.com>
2019-11-22 13:40:55 +0545

    Initial commit

---
+++ app/src/main/java/com/softwarica/tourism/MainActivity.java
@@ -0,0 +1,13 @@
+package com.softwarica.tourism;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
